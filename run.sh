2585b9654a2e4be9c84b0d3c526afc94f53635d8
Gustavo Emerich <gustavo@diviproject.org>
2022-09-21 03:43:56 -0300

    Bump to 0.0.11

--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
 
 # sed -n "s/version\":[[:space:]]*\"//p" package.json
 
-version=${ cat ./package.json | grep -m 1 version | sed 's/[^0-9.]//g'}
-echo "${version}"
+version=$(sed -n "s/version\":[[:space:]]*\"//p" package.json)
 major=0
 minor=0
 revision=0
