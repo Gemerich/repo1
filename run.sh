0aafff4125a329108c09db2f59448628299f9374
Gustavo Emerich <gustavo@diviproject.org>
2022-09-21 02:46:16 -0300

    Bump to 2.1.24235

--- run.sh
+++ run.sh
@@ -1,27 +1,50 @@
-function bump {
-    output=$(npm version ${release} --no-git-tag-version)
-    version=${output:1}
-    search='("version":[[:space:]]*").+(")'
-    replace="\1${version}\2"
+version="$1"
+major=0
+minor=0
+revision=0
+build=0
 
-    sed -i ".tmp" -E "s/${search}/${replace}/g" "$1"
-    rm "$1.tmp"
-}
-
-function help {
-    echo "Usage: $(basename $0) [<newversion> | major | minor | patch | premajor | preminor | prepatch | prerelease]"
-}
-
-if [ -z "$1" ] || [ "$1" = "help" ]; then
-    help
-    exit
+# break down the version number into it's components
+regex="([0-9]+).([0-9]+).([0-9]{2})([0-9]+)"
+if [[ $version =~ $regex ]]; then
+    major="${BASH_REMATCH[1]}"
+    minor="${BASH_REMATCH[2]}"
+    revision="${BASH_REMATCH[3]}"
+    build="${BASH_REMATCH[4]}"
 fi
 
-release=$1
+# check paramater to see which number to increment
+if [[ "$2" == "major" ]]; then
+    major=$(echo $major + 1 | bc)
+    build=$(echo $build + 1 | bc)
+    minor=0
+    revision=00
+elif [[ "$2" == "minor" ]]; then
+    minor=$(echo $minor + 1 | bc)
+    build=$(echo $build + 1 | bc)
+    revision=00
+elif [[ "$2" == "revision" ]]; then
+    revision=$(echo $revision + 1 | bc)
+    build=$(echo $build + 1 | bc)
+elif [[ "$2" == "build" ]]; then
+    build=$(echo $build + 1 | bc)
+else
+    echo "usage: ./version.sh version_number [major/minor/revision/build]"
+    exit -1
+fi
 
-bump "package.json"
+# echo the new version number
+newversion=${major}.${minor}.${revision}${build}
+echo "new version: ${newversion} $2"
+
+search='("version":[[:space:]]*").+(")'
+replace="\1${newversion}\2"
+
+sed -i ".tmp" -E "s/${search}/${replace}/g" "package.json"
+rm "package.json.tmp"
+
 git add .
-git commit -m "Bump to ${version}"
-git tag -a "${output}" -m "${version}"
+git commit -m "Bump to ${newversion}"
+git tag -a "v-${newversion}" -m "${newversion}"
 git push origin --tags
 npm publish ./
